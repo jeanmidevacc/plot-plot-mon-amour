ea833253462efd6c4b20074426cd020f4e0c2f81
Jean-Michel Daignan <jeanmidevacc@gmail.com>
2021-12-14 11:00:52 -0500

    Create python.py
    Add snippet of code

---
+++ python.py
@@ -0,0 +1,10 @@
+fig,ax=plt.subplots(figsize=(12,6))# format of the image
+
+# do your magic around plot
+
+plt.xlabel("Day") # Define the x axis
+plt.ylabel("Count of items released")  # Define the y axis
+
+ax.get_legend().remove() # Drop the legend
+fig.tight_layout() # Extend the plot to the edges of the image
+plt.show()
